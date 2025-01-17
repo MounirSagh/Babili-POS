cfc2a2a674d280b303afbb4d34af525f9e7b4f18
Haytam23 <Haytamraiss.23@gmail.com>
2025-01-17 15:54:12 +0100

    deleting some files src

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import customerRoutes from './routes/customer.routes';
-import discountRoutes from './routes/discount.routes';
-
-app.use('/api/customers', customerRoutes);
-app.use('/api/discounts', discountRoutes); 
